f46004de578f5b94bac4009fce425f110e7298fb
leooleo <leo.ms097@gmail.com>
2019-11-12 21:09:00 -0300

    handshake done

--- src/wsClient.js
+++ src/wsClient.js
@@ -1,14 +1,41 @@
 const WebSocket = require('ws');
-
 const ws = new WebSocket('ws://localhost:8080');
+const crypto = require('./encryptionWrapper')
+
+var [privateKey, publicKey] = crypto.getKeys()
+var serverKey = null
+
+
+function preparePacket() {
+  var pack = crypto.prepareDigitalSignature(privateKey)
+  if(serverKey != null) {
+    pack = crypto.encryptArray(serverKey, pack)    
+  }
+  else {
+    console.log('Server key null')
+    pack = null
+  }
+  return pack
+}
 
 ws.on('open', function open() {
-  ws.send('something');
+  var exportableKey = crypto.exportKey(publicKey)
+  ws.send(exportableKey);
   console.log('sent');
 });
 
-ws.on('message', function incoming(data) {
+ws.on('message', function incoming(message) {
+  if (message.includes('BEGIN PUBLIC KEY')) {
+    serverKey = crypto.importKey(message)
+    var packet = preparePacket()
+    console.log(packet);    
+    console.log(typeof packet);
+    ws.send(packet[0] + '&---&' + packet[1])
+  }
+  else {    
     console.log('received');
-  console.log(data);
-  ws.close();
+    console.log(message);
+    ws.close()
+  }
+
 });
--- src/wsServer.js
+++ src/wsServer.js
@@ -1,10 +1,25 @@
-const WebSocket = require('ws');
- 
-const wss = new WebSocket.Server({ port: 8080 });
- 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message)
-    ws.send(message);
-  }); 
-});
+const WebSocket = require('ws')
+const webSocketServer = new WebSocket.Server({ port: 8080 })
+const crypto = require('./encryptionWrapper')
+
+var [privateKey, pu] = crypto.getKeys()
+var clientKey = null;
+webSocketServer.on('connection', function connection(socket) {
+  socket.on('message', function incoming(message) {
+    if (message.includes('BEGIN PUBLIC KEY')) {
+      clientKey = crypto.importKey(message)
+      var exportableKey = crypto.exportKey(pu)
+      socket.send(exportableKey);
+    }
+    else {
+      var packets = message.split('&---&')
+      var message = crypto.decodeArray(privateKey,packets)
+      message = message.split('<=======')
+
+      var verified = clientKey.verify(message[0], message[1])
+      console.log(verified)
+      socket.send('ok!')
+    }
+
+  })
+})
