8347d383286b1f86e05b7f7f0453c826e609e71f
leooleo <leo.ms097@gmail.com>
2019-11-15 13:12:56 -0300

    basic auth and encryption done

--- server.js
+++ server.js
@@ -11,10 +11,33 @@ const webSocketServer = new WebSocket.Server({ server: server })
 
 var keys = crypto.generateKeyPair()
 var publicKey = crypto.getPublicKey(keys)
-var clientPublicKey = null
+var clients = {}
 
 app.get('/', function (req, res) {
     res.sendFile(root + 'other.html');
+})
+
+app.get('/key/:client', function (req, res) {
+    var client = req.params.client
+    console.log(client);
+    if (client in clients) {
+        var packet = crypto.getPublicKey(clients[client])
+        res.send(packet)
+    }
+    else {
+        res.send('no such client')
+    }
+})
+
+app.get('/client/:client', function (req, res) {
+    var client = req.params.client
+    console.log(client);
+    if (client in clients) {        
+        res.send('error')
+    }
+    else {
+        res.send('ok')
+    }
 })
 
 app.get('/bundle.js', function (req, res) {
@@ -26,13 +49,39 @@ webSocketServer.on('connection', function connection(socket) {
     console.log('Connected')
     socket.on('message', function incoming(message) {
         if (message.includes('BEGIN PUBLIC KEY')) {
-            clientPublicKey = crypto.setPublicKey(message)
+            handlePublicKey(message)
             socket.send(publicKey)
             console.log('Exchanged public keys!')
         }
         else {
-            message = keys.decrypt(message, 'utf-8')
-            console.log(message);
+            handleMessage(message)
         }
     })
 })
+
+function handlePublicKey(message) {
+    message = message.split('user:')
+    var clientPublicKey = crypto.setPublicKey(message[0])
+    var userName = message[1]
+
+    clients[userName] = clientPublicKey
+    console.log('Received public key from client ' + userName)
+}
+
+function handleMessage(message) {
+    try {
+        var decrypted = keys.decrypt(message, 'utf-8')        
+        decrypted = decrypted.split('user:')
+
+        var packet = decrypted[0]
+        var userName = decrypted[1]
+
+        var thisClientKey = clients[userName]
+
+        var finalMessage = thisClientKey.decryptPublic(packet, 'utf-8')
+        console.log('Final message: ' + finalMessage);
+        // var thisClientKey
+    } catch (error) {
+        console.log('Plain text message: ' + message);
+    }
+}
